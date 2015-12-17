dca89d0920e291f2e3edc997af58a78fe81581e8
tokelau <lobckina.nastya@yandex.ru>
2015-12-17 19:19:22 +0800

    считает кол-во символов

---
+++ main.cc
@@ -0,0 +1,61 @@
+//#include <assert.h>
+#include <stdio.h>
+//#include <windows.h>
+
+#define ENCODE_CRUFT 0
+
+// Можно наделать таких блоков для каждого варианта,
+// и потом переключать значения макроса ENCODE_CRUFT
+#if ENCODE_CRUFT == 0
+    #define AP_L 224
+    #define AP_R 255
+    #define CAPS_L 192
+    #define CAPS_R 223
+#endif
+
+int
+main ()
+{
+    /*SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+   // setlocale (LC_TYPE, "rus");*/
+    FILE *qwero;
+    FILE *qwer;
+    int l[33]; /*массив-счетчик*/
+    int i;
+    for (i=0; i<=32; i++)
+    {
+        l[i]=0;
+    };
+
+    int c;
+        qwero=fopen ("C:\\qwero.txt", "w");
+        qwer=fopen("C:\\qwer.txt", "r");
+
+    while ((c=fgetc(qwer)) != EOF) /*распределение и подсчет символов*/
+    {
+        printf("%d\t", c);
+        if (c>=AP_L && c <=AP_R) /* а-п */
+        {
+            c-=AP_L-1;
+            l[c]++;
+            //printf("%d\n",l[c]);
+        }
+                if (c>=CAPS_L && c<= CAPS_R) /* А-Я */
+        {
+            c-=CAPS_L-1;
+            l[c]++;
+            //printf("%d\n", l[c]);
+        }
+    }
+    printf("%d\n", l[2]); /*почему не печатается??? */
+
+       for (i=0; i<=32; i++)
+    {
+        fprintf(qwero, "%d\n",l[i]);
+    }
+    //assert (! ferror (stdin));
+    fclose (qwero);
+    fclose (qwer);
+    return 0;
+    }
